1edbfd9bb1ceb35193669b1240d9331900c5f64d
Pedro Braga <peradi2001@gmail.com>
2025-07-13 20:21:18 +0100

    feat: send training/forecasting results to backend queue

--- rabbit_queue.py
+++ rabbit_queue.py
@@ -23,6 +23,7 @@ class RabbitQueue:
         self.channel = self.connection.channel()
         
         self.queue_name = f'queue_{type}'
+        self.backend_queue_name = os.getenv('RABBITMQ_BACKEND_QUEUE')
 
         if type == 'trainer':
             self.op = Trainer(db, device)
@@ -30,15 +31,15 @@ class RabbitQueue:
             self.op = Forecaster(db, device)
             
         self.channel.queue_declare(queue=self.queue_name)
+        self.channel.queue_declare(queue=self.backend_queue_name, durable=True)
         
         self.channel.basic_consume(
             queue=self.queue_name,
             on_message_callback=self.on_request)
 
     def on_request(self, ch, method, props, body):
-        payload = json.loads(body)
-
-        result = self.exec(payload["payload"])
+        data = json.loads(body)["data"]
+        job_id = data["job_id"]
         
         ch.basic_publish(
             exchange='',
@@ -46,16 +47,40 @@ class RabbitQueue:
             properties=pika.BasicProperties(
                 correlation_id=props.correlation_id
             ),
-            body=json.dumps(result))
-        
+            body=(json.dumps({"status": "PROCESSING"}))
+        )
         ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        result = self.exec(data)
+        self.post_to_backend_queue(job_id, result)
 
     def exec(self, payload):
         try:
             result = self.op.exec(payload)
+            return {"success": True, "data": result}
         except Exception as e:
-            result = {"success": False, "error": str(e)}
-        return {"success": True, "result": result}
+            return {"success": False, "error": str(e)}
+    
+    def post_to_backend_queue(self, job_id, message):
+        try:
+            message = {
+                "pattern": self.type,
+                "data": {
+                    "job_id": job_id,
+                    "result": message
+                }
+            }
+
+            self.channel.basic_publish(
+                exchange='', 
+                routing_key=os.getenv('RABBITMQ_BACKEND_QUEUE'),
+                body=json.dumps(message),
+                properties=pika.BasicProperties(
+                    delivery_mode=2,
+                )
+            )
+        except Exception as e:
+            print(f"Error posting to backend queue: {str(e)}")
     
     def start(self):
         print(f"<!> Node listening on {self.queue_name}")
